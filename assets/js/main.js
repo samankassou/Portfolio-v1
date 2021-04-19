735033665602ae86781b9370a57ee7e50214fb6a
samankassou <samankassoufoulla@gmail.com>
2021-04-19 20:18:19 +0200

    Add aos and gsap javascript libraries for animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+AOS.init();
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
@@ -15,3 +16,11 @@ function mobileMenu() {
 
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+// gsap animation
+gsap.from(".nav-logo", {duration: 2, x: -100});
+gsap.from(".nav-item", {opacity: 0, duration: 1, delay: 1.5, y: 30, stagger: 0.2});
+
+gsap.from(".home__presentation", {opacity: 0, duration: 1, delay: 1, y: 30});
+gsap.from(".home__hello", {opacity: 0, duration: 1, delay: 1.6, y: 30});
+gsap.from(".home__image-box", {opacity: 0, duration: 1, delay: 1.8, y: 30});
