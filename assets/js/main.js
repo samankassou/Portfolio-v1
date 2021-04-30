0a06c80aab9b810b4c9627cd3aa87f5ecb41ec13
samankassou <samankassoufoulla@gmail.com>
2021-04-30 07:51:02 +0200

    Add the send email function

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,9 @@ AOS.init();
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
+const sendEmailBtn = document.querySelector('#send-email-btn');
+
+sendEmailBtn.addEventListener('click', sendEmail);
 
 
 function closeMenu() {
@@ -24,3 +27,12 @@ gsap.from(".nav-item", {opacity: 0, duration: 1, delay: 1.5, y: 30, stagger: 0.2
 gsap.from(".home__presentation", {opacity: 0, duration: 1, delay: 1, y: 30});
 gsap.from(".home__hello", {opacity: 0, duration: 1, delay: 1.6, y: 30});
 gsap.from(".home__image-box", {opacity: 0, duration: 1, delay: 1.8, y: 30});
+
+function sendEmail(){
+    const name = document.querySelector('#name');
+    const email = document.querySelector('#email');
+    const subject = document.querySelector('#subject');
+    const body = document.querySelector('#body');
+
+    window.open(`mailto:samankassoufoulla@gmail.com?name=${name}&subject=${subject}&body=${body}`);
+}
